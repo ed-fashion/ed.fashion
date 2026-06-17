2e6b4f10a2a6106440c7cdf9cc47c538e1b641e1
ed-fashion <ed.ffashion@gmail.com>
2026-06-17 10:51:50 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,3 @@
-// ============================================================
-// E'D FASHION — Supabase Client
-// ============================================================
-
 const SUPABASE_URL = 'https://sabvkhcjmlgugypqkusn.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNhYnZraGNqbWxndWd5cHFrdXNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2NTQ0MDQsImV4cCI6MjA5NzIzMDQwNH0.zFaGDcTa3sPxEwqcS8AzVdnhgq6QUG4qP_GKIVpLXlo';
 
